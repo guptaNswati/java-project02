d17a47104c914b67959bd6cd68cd50095dc1f0a7
swati gupta <sgupta1505@gmail.com>
2016-04-14 19:46:05 -0700

    Trying to commit the subsetsum class

--- src/subsetsum/SubsetSum.java
+++ src/subsetsum/SubsetSum.java
@@ -1,93 +1,206 @@
 package subsetsum;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import cs1c.SongEntry;
+
+/**
+ * Given the condition of how much the user's target is, determines a list of grocery items and songs.
+ * @author swati
+ *
+ */
 public class SubsetSum
 {
-    public static ArrayList<ArrayList<Double>> listOfList = new ArrayList<ArrayList<Double>>();
-    
-    public static double sumFunction(ArrayList<Double> list)
+    /**
+     * calculates the sum of elements of given arraylist
+     * @param list [a generic arraylist]
+     * @return sum of elements of list
+     */
+    public static double sumFunction(ArrayList<?> list)
     {
-      if (list.isEmpty())
-            return 0;
-      
-          double sum = 0;           
+        double sum = 0;     
 
-          for(int i = 0; i < list.size(); i++) 
-          {                                
-              sum = sum + list.get(i);                                      
-          }
-          return sum;          
+        if (list.isEmpty())
+        {
+            return 0;
+        }
+
+        // check if list contains Objects of type double
+        else if (list.get(0) instanceof Double)
+        {                        
+            for(int i = 0; i < list.size(); i++) 
+            {  
+                double item = (double) list.get(i);
+                sum = sum + item ;                                    
+            }
+            return sum; 
+        }
+
+        // check if list contains SongEntry Objects
+        else if (list.get(0) instanceof SongEntry)
+        {
+            ArrayList<SongEntry> songList = (ArrayList<SongEntry>) list;
+
+            for(int i = 0; i < list.size(); i++)
+            {         
+                double duration = songList.get(i).getDuration();
+                sum = sum + duration;
+            }
+            return sum;
+        }
+        return sum;
     }
-        
-    public static ArrayList<Double> findSubset(ArrayList<Double> list, double budget)
-    {   // collection of all possible lists                 
+
+    /**
+     * 
+     * @param list [ArrayList containing Objects of Type Double]
+     * @param budget [users target amount in double]
+     * @return list of items within budget
+     */
+    public static ArrayList<Double> creatingGroceryList(ArrayList<Double> list, double budget)
+    {
+        // collection of subsets
         ArrayList<ArrayList<Double>> listOfLists = new ArrayList<ArrayList<Double>>();        
-        
+
         // an empty list
         ArrayList<Double> subList = new ArrayList<Double>();
         listOfLists.add(subList);
-        
+
         // list for closest match to the budget
         ArrayList<Double> closestSubList = new ArrayList<Double>();
-       
+
+        if (budget > sumFunction(list))
+        {
+            return list;                      
+        }
+        else
+            for (int i = 0; i < list.size(); i++)
+            {                                                  
+                int listSize = listOfLists.size();
+                int index = 0;                 
+
+                while (index < listSize)                    
+                {                    
+                    ArrayList<Double> currentSet = listOfLists.get(index);
+
+                    double setSum = 0;
+
+                    setSum = sumFunction(currentSet) + (double)list.get(i);
+
+                    if(setSum <= budget)                
+                    {
+                        ArrayList<Double> subSet = new ArrayList<Double>();
+
+                        for(int m = 0 ; m < currentSet.size() ; m++)
+                        {
+                            subSet.add(currentSet.get(m));                            
+                        }                        
+                        subSet.add(list.get(i));                                                
+                        listOfLists.add(subSet);
+
+                        if(setSum == budget)                    
+                        {                            
+                            return  subSet;
+                        }
+                        // for closest match 
+                        else if (sumFunction(closestSubList) < setSum)
+                        {
+                            closestSubList = subSet;
+                        }
+                    }  // ending of if(setSum <= budget) block
+
+                    index++;                     
+
+                } // ending while block                              
+
+            }// ending outer for loop                      
+
+        return closestSubList;       
+    }
+
+    /**
+     * 
+     * @param list[ArrayList containing Objects of Type SongEntry]
+     * @param budget [users target duration in double]
+     * @return list of song within duration
+     */
+    public static ArrayList<SongEntry> creatingSongList(ArrayList<SongEntry> list, double budget)
+    { 
+        // collection of subsets
+        ArrayList<ArrayList<SongEntry>> listOfLists = new ArrayList<ArrayList<SongEntry>>();        
+
+        // an empty list
+        ArrayList<SongEntry> subList = new ArrayList<SongEntry>();
+        listOfLists.add(subList);
+
+        // list for closest match to the budget
+        ArrayList<SongEntry> closestSubList = new ArrayList<SongEntry>();
+
         if (budget > sumFunction(list))
         {
             return list;                      
         }
 
-        for (int i = 0; i < list.size(); i++)
-        {                                                  
-            int k = listOfLists.size();
-            int j = 0;                 
+        else
+            for (int i = 0; i < list.size(); i++)
+            {                                                  
+                int listSize = listOfLists.size();
+                int index = 0;  
 
-            while (j < k)                    
-            {                    
-                ArrayList<Double> currentSet = listOfLists.get(j);
-                
-                double setSum = sumFunction(currentSet) + list.get(i);                                
-                
-                if(setSum <= budget)
-                {
-                    ArrayList<Double> subSet = new ArrayList<Double>();
-                                           
-                    for(int m = 0 ; m < currentSet.size() ; m++)
+                while (index < listSize)                    
+
+                {                    
+                    ArrayList<SongEntry> currentSet = listOfLists.get(index);
+
+                    double setSum = 0;
+
+                    setSum = sumFunction(currentSet) + (double)list.get(i).getDuration();                
+
+                    if(setSum <= budget)                
                     {
-                        subSet.add(currentSet.get(m));                            
-                    }                        
-                    subSet.add(list.get(i));                                                
-                    listOfLists.add(subSet);
-                        
-                    if(setSum == budget)                    
-                    {                            
-                        return  subSet;
-                    }
-                    // for closest match 
-                    else if (sumFunction(closestSubList) < setSum)
-                    {
-                        closestSubList = subSet;
-                    }
-                } 
-                j++;                                                            
-            }                 
-            
-         }
-        listOfList = listOfLists;
-        return closestSubList;                 
-        }
-    
-    public static ArrayList<ArrayList<Double>> memoryOptimization(ArrayList<ArrayList<Double>> listOfLists, ArrayList<Double> list)
+                        ArrayList<SongEntry> subSet = new ArrayList<SongEntry>();
+
+
+                        for(int m = 0 ; m < currentSet.size() ; m++)
+                        {
+                            subSet.add(currentSet.get(m));                            
+                        }                        
+                        subSet.add(list.get(i));                                                
+                        listOfLists.add(subSet);
+
+                        if(setSum == budget)                    
+                        {                            
+                            return  subSet;
+                        }
+                        // for closest match 
+                        else if (sumFunction(closestSubList) < setSum)
+                        {
+                            closestSubList = subSet;
+                        }
+                    } // ending of if(setSum <= budget) block
+
+                    index++;                                                            
+
+                } // ending while block                              
+
+            } // ending outer for loop 
+
+        return closestSubList;              
+    }
+
+    /**
+     * check whether the list contains Objects of SongEntry or Double and creates a list accordingly
+     * @param list [generic ArrayList]
+     * @param budget [users's target]
+     * @return a generic list of items within target
+     */
+    public static ArrayList<?> findSubset(ArrayList<?> list, double budget)
     {
-        for(int i = 0; i < listOfLists.size(); i++)
+        if (list.get(0) instanceof Double)
         {
-            if(list.subList(0, list.size()-1).equals(listOfLists.get(i)))
-            {
-                listOfLists.remove(listOfLists.get(i));
-//                break;
-            }
+            return creatingGroceryList((ArrayList<Double>) list, budget);           
         }
-        return listOfLists; 
-        
+        else
+            return creatingSongList((ArrayList<SongEntry>) list, budget);
     }
 }
